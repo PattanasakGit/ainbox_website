7022df4929fc9f5312cde92638df89e0aea2ce42
Pattanasak <Putjat145@gmail.com>
2024-05-20 21:48:17 +0700

    fix: fix size for  normal display

--- landing-page/src/components/Home/Home.tsx
+++ landing-page/src/components/Home/Home.tsx
@@ -5,16 +5,15 @@ import React from "react";
 
 const Home: React.FC = () => {
     return (
-        <section className="pt-[200px] w-full p-4 text-center">
+        <section className="pt-[70px] w-full p-4 text-center">
           <div className="h-[600px] w-full mb-16">
-          <div className="h-full w-[60%] mx-auto bg-[#fff] rounded-3xl flex items-center justify-center text-[30px] text-white relative">
-           {/* <Image src={ainboxData.home.images[0]} alt='24/7' layout="fill" objectFit="cover" /> */}
-           {/* <Image src={ainboxData.home.images[0]} alt='24/7' height={600} width={600} /> */}
-           <img src={ainboxData.home.images[0]} alt='24/7' height={200} width={600} />
+            <div className="h-full w-[60%] mx-auto bg-[#fff] rounded-3xl flex items-center justify-center text-[30px] text-white relative">
+            {/* <Image src={ainboxData.home.images[0]} alt='24/7' layout="fill" objectFit="cover" /> */}
+            {/* <Image src={ainboxData.home.images[0]} alt='24/7' height={600} width={600} /> */}
+            <img src={ainboxData.home.images[0]} alt='24/7' height={200} width={450} />
+            </div>
           </div>
-
-          </div>
-          <div className="mt-[100px]">
+          <div>
             <h1 className="topic text-[40px] my-6">
               {ainboxData.home.topic}
             </h1>
@@ -35,7 +34,7 @@ const Home: React.FC = () => {
                             <Image src={ainboxData.home.images[1]} alt='24/7' width={300} height={300}/>
                         </div>
                         <div className="w-full lg:w-[70%] bg-[#F1F9FF] rounded-3xl p-8">
-                            <h1 className="topic text-[40px] my-6">
+                            <h1 className="topic text-[30px] my-6">
                                 {ainboxData.home.details[0]}
                             </h1>
                             <p className="textDetail ">
@@ -45,7 +44,7 @@ const Home: React.FC = () => {
                     </div>
                     <div className=" flex flex-wrap items-center justify-center p-10">
                         <div className="w-full lg:w-[70%] bg-[#fffff1] rounded-3xl p-8">
-                            <h1 className="topic text-[40px] my-6">
+                            <h1 className="topic text-[30px] my-6">
                                 {ainboxData.home.details[2]}
                             </h1>
                             <p className="textDetail ">
--- landing-page/src/components/NavBar/Navbar.tsx
+++ landing-page/src/components/NavBar/Navbar.tsx
@@ -5,17 +5,17 @@ import { Link } from "react-scroll";
 
 const CustomNavbar: React.FC = () => {
   return (
-    <nav className="h-[100px] flex justify-between items-center w-full px-8 fixed top-0 z-[1000] bg-[#fffbf5]">
-      <Image src={ainboxData.img.logo} alt="logo" width={200} height={200} />
+    <nav className="h-[70px] flex justify-between items-center w-full px-8 fixed top-0 z-[1000] bg-[#fffbf5]">
+      <Image src={ainboxData.img.logo} alt="logo" width={180} height={180} />
 
-      <div className="topic text-[25px] flex justify-evenly w-1/2">
+      <div className="topic text-[20px] flex justify-evenly w-1/2">
         <div> Home </div>
         <div> Features </div>
         <div> Integrations </div>
         <div> Pricing </div>
       </div>
 
-      <button className="font-semibold text-[25px] py-3 px-4 rounded-2xl text-white bg-gradient-to-b from-[#FB8854] to-[#F9373C]
+      <button className="font-semibold text-[20px] py-2 px-4 rounded-2xl text-white bg-gradient-to-b from-[#FB8854] to-[#F9373C]
         hover:scale-[1.1]">
         Start Now
       </button>
