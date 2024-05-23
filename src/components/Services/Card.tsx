5d17cb0158829d3849bb7a1125d39ec74d45d515
Pattanasak <Putjat145@gmail.com>
2024-05-23 11:34:20 +0700

    fix: add motion in services

--- src/components/Services/Card.tsx
+++ src/components/Services/Card.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+"use client"
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { IFeaturesData } from '@/models/IFeaturesData';
-import { FaCheck } from "react-icons/fa";
-import { IoCheckmarkDoneSharp } from "react-icons/io5";
-import { IoInformationCircleOutline } from "react-icons/io5";
+import { IoCheckmarkDoneSharp, IoInformationCircleOutline } from "react-icons/io5";
 import { BsStars } from "react-icons/bs";
+import { Variants, motion } from "framer-motion";
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 20,
+    opacity: 0,
+    scale: 0.8,
+  },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.8,
+      bounce: 0.5,
+    },
+  },
+};
 
 interface CardProps {
   data: IFeaturesData;
@@ -11,27 +29,67 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ data }) => {
   const { title, features, example } = data;
+  const cardRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  useLayoutEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          } else {
+            setIsVisible(false);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    observerRef.current = observer;
+
+    return () => {
+      if (observerRef.current && cardRef.current) {
+        observerRef.current.unobserve(cardRef.current);
+      }
+    };
+  }, []);
 
   return (
-      <div className="bg-[#ffffff] p-6 rounded-3xl shadow-2xl shadow-[#bd876555] border-[#df89427b]">
-        <div className='flex items-center'>
+    <motion.div
+      ref={cardRef}
+      initial="offscreen"
+      animate={isVisible ? 'onscreen' : 'offscreen'}
+      variants={cardVariants}
+      className="bg-[#ffffff] p-6 rounded-3xl shadow-2xl shadow-[#bd876555] border-[#df89427b]"
+    >
+      <div className='flex items-center'>
         <BsStars className='text-[#eed442]' />
         <h2 className="topic text-xl font-bold mb-2 text-center mx-2">{title}</h2>
         <hr className="flex-grow h-[2px] bg-gradient-to-r from-[#ffe079b8] to-[#ffffff] border-none rounded-xl" />
       </div>
       <div className="bg-[#ffffffaf] px-4 py-6 rounded-2xl border-2 border-[#f7c89c43]">
-        {/* <hr className="h-[2px] w-[85%] bg-[#b5a39239] border-none rounded-xl mx-auto mb-4" /> */}
         <ul className="list-disc">
           {features.map((feature) => (
             <li key={feature} className="flex items-start">
-              <div className="mr-2 flex items-center justify-center"> <IoCheckmarkDoneSharp className='text-[#489727] mt-1'/></div>
+              <div className="mr-2 flex items-center justify-center">
+                <IoCheckmarkDoneSharp className='text-[#489727] mt-1'/>
+              </div>
               <span className='font-fam-only text-[#555]'>{feature}</span>
             </li>
           ))}
         </ul>
       </div>
-      <p className="font-fam-only text-[12px] mt-4 text-[#555] flex items-start"> <IoInformationCircleOutline className='mr-1' size={20} /> {example} </p>
-    </div>
+      <p className="font-fam-only text-[12px] mt-4 text-[#555] flex items-start">
+        <IoInformationCircleOutline className='mr-1' size={20} />
+        {example}
+      </p>
+    </motion.div>
   );
 };
 
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -10,7 +10,7 @@ const Services: React.FC = () => {
       <Title text={'SERVICES'} />
       <p className='font-fam-only text-[#555] text-[18px] text-center mb-4 '> <b> Chatbot AI 🤖 ใคร ๆ ก็ทำได้ </b>  ต่อไปนี้คือตัวอย่างที่คุณเองก็ทำได้ ง่ายนิดเดียว 🤏🏻  </p>
       <div className='w-full flex justify-center'>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 w-[95%]  p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 w-[95%] lg:w-[85%]  p-4">
           {FeaturesData.map((data: IFeaturesData) => (
             <Card key={data.title} data={data} />
           ))}
