31706a571765ecf54b7e59feaa871817104aae0b
Pattanasak <Putjat145@gmail.com>
2024-05-25 22:20:59 +0700

    fix: fix navBar action scoll to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
+import React from "react";
 import Example from "@/components/Example/Example";
 import FAQComponent from "@/components/FAQ/FAQComponent";
 import Home from "@/components/Home/Home";
 import Price from "@/components/Price/Price";
+import CustomNavbar from "@/components/NavBar/Navbar";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Home/>
-      <Example/>
-      <FAQComponent/>
-      <Price/>
-    </main>
+    <>
+      <CustomNavbar />
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <section id="home">
+          <Home />
+        </section>
+        <section id="example">
+          <Example />
+        </section>
+        <section id="faq">
+          <FAQComponent />
+        </section>
+        <section id="price">
+          <Price />
+        </section>
+      </main>
+    </>
   );
 }
--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,31 +1,65 @@
+"use client";
 import { ainboxData } from "@/store/HomeData";
 import Image from "next/image";
+import { Link } from 'react-scroll';
 import React from "react";
 
 const CustomNavbar: React.FC = () => {
+  const textColorActive = 'text-orange-500'
+  const styleBtnNavbar = 'hover:text-orange-600 cursor-pointer'
   return (
-    <nav className="h-[70px] flex justify-between items-center w-full px-8 fixed top-0 z-[1000] bg-[#ffffff] shadow-lg">
+    <nav className="h-[70px] flex justify-between items-center w-full px-8 fixed top-0 z-[1000] bg-[#ffffff] shadow-sm">
       <Image src={ainboxData.img.logo} alt="logo" width={180} height={180} />
-
       <div className="topic text-[20px] flex justify-evenly w-1/2">
-        {/* <div> Home </div>
-        <div> Features </div>
-        <div> Integrations </div>
-        <div> Pricing </div> */}
-        <div>บริการของเรา</div>
-        <div>ตัวอย่างการใช้งาน</div>
-        <div>คำถามยอดนิยม</div>
-        <div>ราคา</div>
+        <Link
+          to="home"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className={styleBtnNavbar}
+          activeClass={textColorActive}
+        >
+          บริการของเรา
+        </Link>
+        <Link
+          to="example"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className={styleBtnNavbar}
+          activeClass={textColorActive}
+        >
+          ตัวอย่างการใช้งาน
+        </Link>
+        <Link
+          to="faq"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className={styleBtnNavbar}
+          activeClass={textColorActive}
+        >
+          คำถามยอดนิยม
+        </Link>
+        <Link
+          to="price"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className={styleBtnNavbar}
+          activeClass={textColorActive}
+        >
+          ราคา
+        </Link>
       </div>
-
       <button
-        className="font-semibold text-[20px] py-2 px-4 rounded-2xl text-white bg-gradient-to-b from-[#FB8854] to-[#F9373C]
-        hover:scale-[1.1]"
+        className="font-semibold text-[19px] py-2 px-4 rounded-xl text-white bg-gradient-to-b from-[#FB8854] to-[#F9373C] hover:scale-[1.1]"
       >
-        Start Now
+        เข้าสู่ระบบ
       </button>
     </nav>
   );
 };
 
 export default CustomNavbar;
+
