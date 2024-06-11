7fdc103536282b36f6dfd6fb85e0fc96eaefdd50
Pattanasak <Putjat145@gmail.com>
2024-06-12 05:27:16 +0700

    Fix: fix bug sessionStorage on SSR

--- frontend/portal/src/store/dataChannel.ts
+++ frontend/portal/src/store/dataChannel.ts
@@ -1,23 +1,30 @@
+"use client"
 import { create } from 'zustand'
 import { IStore } from '@/models/IChannel'
 
 interface IdataChannel {
-  dataChannel: IStore | null; 
-  setDataChannel: (value: IStore) => void; 
+  dataChannel: IStore | null;
+  setDataChannel: (value: IStore) => void;
 }
 
 const getInitialState = (): IStore | null => {
-  const storedState = sessionStorage.getItem('dataChannel');
-  return storedState ? JSON.parse(storedState) : null;
+  if (typeof window !== 'undefined') {
+    const storedState = sessionStorage.getItem('dataChannel');
+    return storedState ? JSON.parse(storedState) : null;
+  }
+  return null;
 };
 
 export const useDataChannel = create<IdataChannel>((set) => ({
   dataChannel: getInitialState(),
   setDataChannel: (value: IStore) => {
     set({ dataChannel: value });
-    sessionStorage.setItem('dataChannel', JSON.stringify(value));
-  }, 
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('dataChannel', JSON.stringify(value));
+    }
+  },
 }));
+
 
 
 
